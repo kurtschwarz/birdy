8874e45cb4aa96a1a8a91f4c389cde6dfd414208
Kurt Schwarz <kurt@unkn.co>
2024-04-19 01:40:47 +0000

    feat(@birdy/recorder): adds cli framework to configure the analzyer

--- services/analyzer/src/main.py
+++ services/analyzer/src/main.py
@@ -6,6 +6,7 @@ import grpc
 import aiomqtt
 import json
 import logging
+import click
 
 from minio import Minio
 from birdnetlib import RecordingFileObject
@@ -16,6 +17,9 @@ sys.path.insert(0, "../../../packages/protos/gen/py")
 
 from analyzer.v1 import service_pb2 as analyzer_pb2
 from analyzer.v1 import service_pb2_grpc as analyzer_pb2_grpc
+
+
+logging.basicConfig(level=logging.INFO)
 
 storage = Minio(
     "%s:%s" % (os.environ["MINIO_ENDPOINT"], os.environ["MINIO_PORT"]),
@@ -92,7 +96,7 @@ background_tasks = set()
 cleanup_tasks = []
 
 
-async def main():
+async def start():
     logging.info("starting")
 
     loop = asyncio.get_event_loop()
@@ -115,12 +119,47 @@ async def main():
     await server.wait_for_termination()
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+def configure(ctx, param, file_path):
+    pass
 
+
+def configureLogger(ctx, param, level):
+    levels = logging.getLevelNamesMapping()
+    if level.upper() in levels:
+        logging.getLogger().setLevel(levels[level.upper()])
+
+
+@click.group(invoke_without_command=True)
+@click.option(
+    "-c",
+    "--config",
+    type=click.Path(dir_okay=False),
+    callback=configure,
+    is_eager=True,
+    expose_value=False,
+    show_default=False,
+)
+@click.option(
+    "--log-level",
+    type=click.Choice(
+        choices=list(
+            map(lambda key: key.lower(), logging.getLevelNamesMapping().keys())
+        )
+    ),
+    default="info",
+    callback=configureLogger,
+    is_eager=True,
+    expose_value=False,
+    show_default=False,
+)
+def main(**kwargs):
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(main())
+        loop.run_until_complete(start())
     finally:
         loop.run_until_complete(*cleanup_tasks)
         loop.close()
+
+
+if __name__ == "__main__":
+    main()
